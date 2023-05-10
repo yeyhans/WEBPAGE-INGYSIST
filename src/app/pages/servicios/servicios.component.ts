67510cc9da68bb7b947abad043664f0c2f697a27
Yeyson Hans <yeysonhans@gmail.com>
2023-05-10 19:42:17 -0400

    Se creo bien la ruta de imagenes del proyecto

--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -12,9 +12,9 @@ export class ServiciosComponent implements OnInit {
   constructor( ){
 
     this.servicios = [
-      {id: 1, ruta:"/formulacion-de-proyectos-para-fondos-concursables", titulo:"Formulación de proyectos para fondos concursables", urlImg:"/assets/img/formulaciondeproyecto.jpg", descripcion:"Descubre la financiación ideal para tu proyecto con nuestro servicio de análisis de ideas y fondos de financiamiento."},
-      {id: 2, ruta: "/diseño-y-desarrollo-de-sistemas-informáticos", titulo:"Diseño y desarrollo de sistemas informáticos.", urlImg:"/assets/img/disenoinformatico.jpg", descripcion:"Transforma tus ideas en sistemas innovadores y gana financiamiento con nuestro equipo de expertos."},
-      {id: 3, ruta: "/modelamiento-de-procesos", titulo:"Modelamiento de procesos.", urlImg:"/assets/img/modelamientoproceso.jpg", descripcion:"Optimiza tus procesos y mejora el rendimiento de tu empresa con nuestro equipo de ingenieros industriales especializados."}
+      {id: 1, ruta:"./formulacion-de-proyectos-para-fondos-concursables", titulo:"Formulación de proyectos para fondos concursables", urlImg:"/assets/img/formulaciondeproyecto.jpg", descripcion:"Descubre la financiación ideal para tu proyecto con nuestro servicio de análisis de ideas y fondos de financiamiento."},
+      {id: 2, ruta: "./diseño-y-desarrollo-de-sistemas-informáticos", titulo:"Diseño y desarrollo de sistemas informáticos.", urlImg:"/assets/img/disenoinformatico.jpg", descripcion:"Transforma tus ideas en sistemas innovadores y gana financiamiento con nuestro equipo de expertos."},
+      {id: 3, ruta: "./modelamiento-de-procesos", titulo:"Modelamiento de procesos.", urlImg:"/assets/img/modelamientoproceso.jpg", descripcion:"Optimiza tus procesos y mejora el rendimiento de tu empresa con nuestro equipo de ingenieros industriales especializados."}
     ];
 
 
